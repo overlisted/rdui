b771355e9e103d8b173b0dff4854369f30b448e4
Ilya Maximov <mail@overlisted.net>
2020-11-09 23:16:51 +0100

    add rectangle's borders rendering function

--- util.h
+++ util.h
@@ -1,11 +1,19 @@
 #pragma once
 
 #include <stddef.h>
+#include "rawdraw/CNFG.h"
 
 char* UtilStringInsert(char* original, size_t index, char* to_insert);
 char* UtilStringRemove(char* original, size_t from, size_t to);
 char* UtilStringAppend(char* original, char* to_append);
 
+void UtilDrawRectangleBorders(int width, int x1, int y1, int x2, int y2) {
+	CNFGSetLineWidth(width);
+	CNFGTackSegment(x1, y1, x2, y1);
+	CNFGTackSegment(x2, y1, x2, y2);
+	CNFGTackSegment(x2, y2, x1, y2);
+	CNFGTackSegment(x1, y2, x1, y1);
+}
 
 int UtilMax(int a, int b) {
 	return a > b ? a : b;
