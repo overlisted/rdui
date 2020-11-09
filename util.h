80b4d1e649a884264fe50e69685cfcdd40183ab8
Ilya Maximov <mail@overlisted.net>
2020-11-09 23:15:59 +0100

    add UtilMax function

--- util.h
+++ util.h
@@ -5,3 +5,8 @@
 char* UtilStringInsert(char* original, size_t index, char* to_insert);
 char* UtilStringRemove(char* original, size_t from, size_t to);
 char* UtilStringAppend(char* original, char* to_append);
+
+
+int UtilMax(int a, int b) {
+	return a > b ? a : b;
+}
