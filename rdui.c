4bc08a61a23b22d67530b2e764288aa7c95633ee
Ilya Maximov <mail@overlisted.net>
2020-11-30 13:26:31 +0100

    change the way you store elements

--- rdui.c
+++ rdui.c
@@ -1,15 +1,11 @@
 #include "RDUI.h"
 
+#include <stdarg.h>
+
 RDPoint RDUIMousePosition = {.x = 0, .y = 0};
 
-static struct {
-	struct RDUINode** array;
-	size_t count;
-} Nodes;
-
 void RDUIInit() {
-	Nodes.array = malloc(0);
-	Nodes.count = 0;
+	// may be removed in future
 }
 
 struct RDUINode* RDUINewNode(
@@ -24,40 +20,54 @@ struct RDUINode* RDUINewNode(
 	return node;
 }
 
-void RDUIPushNode(struct RDUINode* node) {
-	Nodes.count++;
+struct RDUIMenu* RDUINewMenu(size_t size, ...) {
+	va_list list;
 
-	Nodes.array = realloc(Nodes.array, Nodes.count * sizeof(struct RDUINode*));
-	if(Nodes.array != NULL) {
-    Nodes.array[Nodes.count - 1] = node;
+	struct RDUIMenu* menu = malloc(sizeof(struct RDUIMenu));
+	menu->size = size;
+	menu->nodes = malloc(sizeof(struct RDUINode*) * size);
+
+	va_start(list, size);
+	for(int i = 0; i < size; i++) menu->nodes[i] = va_arg(list, struct RDUINode*);
+	va_end(list);
+
+	return menu;
+}
+
+void RDUIMenuPush(struct RDUIMenu* menu, struct RDUINode* node) {
+	menu->size++;
+
+	menu->nodes = malloc(menu->size * sizeof(struct RDUINode*));
+	if(menu->nodes != NULL) {
+    menu->nodes[menu->size - 1] = node;
   } else {
-    Nodes.count--;
+    menu->size--;
   }
 }
 
-#define ForEachNode() for(size_t i = 0; i < Nodes.count; i++)
+#define ForEachNode() for(size_t i = 0; i < menu->size; i++)
 
-void RDUIDispatchEvent(enum RDUIEvent event, void* data) {
-	ForEachNode() Nodes.array[i]->event_receiver_function(Nodes.array[i], event, data);
+void RDUIDispatchEvent(struct RDUIMenu* menu, enum RDUIEvent event, void* data) {
+	ForEachNode() menu->nodes[i]->event_receiver_function(menu->nodes[i], event, data);
 }
 
 void RDUINoOpEventReceiver(struct RDUINode* node, enum RDUIEvent event, void* data) {}
 
-void RDUIHandleKeyImpl(int keycode, int bDown) {
+void RDUIHandleKeyImpl(struct RDUIMenu* menu, int keycode, int bDown) {
 	struct RDUIEventData_key event = {.keycode = keycode, .bDown = bDown};
-	RDUIDispatchEvent(RDUIEvent_key, &event);
+	RDUIDispatchEvent(menu, RDUIEvent_key, &event);
 }
 
-void RDUIHandleButtonImpl(int x, int y, int button, int bDown) {
+void RDUIHandleButtonImpl(struct RDUIMenu* menu, int x, int y, int button, int bDown) {
 	struct RDUIEventData_button event = {.position = {.x = x, .y = y}, .button = button, .bDown = bDown};
-	RDUIDispatchEvent(RDUIEvent_button, &event);
+	RDUIDispatchEvent(menu, RDUIEvent_button, &event);
 }
 
-void RDUIHandleMotionImpl(int x, int y, int mask) {
+void RDUIHandleMotionImpl(struct RDUIMenu* menu, int x, int y, int mask) {
 	struct RDUIEventData_motion event = {.position = {.x = x, .y = y}, .mask = mask};
 
 	RDUIMousePosition.x = x;
 	RDUIMousePosition.y = y;
 
-	RDUIDispatchEvent(RDUIEvent_motion, &event);
+	RDUIDispatchEvent(menu, RDUIEvent_motion, &event);
 }
