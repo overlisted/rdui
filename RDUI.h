aa7f2851dd59677053b5cd1cc116f18f2e78ec4a
Ilya Maximov <mail@overlisted.net>
2021-06-11 22:21:46 +0200

    reformat everything

--- RDUI.h
+++ RDUI.h
@@ -1,130 +1,133 @@
 #pragma once
 
-#include <stdlib.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
 #include "rawdraw/CNFG.h"
 
 enum RDUIEvent {
-	RDUIEvent_render,
-	RDUIEvent_key,
-	RDUIEvent_button,
-	RDUIEvent_motion
+  RDUIEvent_render,
+  RDUIEvent_key,
+  RDUIEvent_button,
+  RDUIEvent_motion
 };
 
 struct RDUIEventData_key {
-	int keycode;
-	int bDown;
+  int keycode;
+  int bDown;
 };
 
 struct RDUIEventData_button {
-	RDPoint position;
-	int button;
-	int bDown;
+  RDPoint position;
+  int button;
+  int bDown;
 };
 
 struct RDUIEventData_motion {
-	RDPoint position;
-	int mask;
+  RDPoint position;
+  int mask;
 };
 
 struct RDUINode;
 
-typedef void (*RDUIEventReceiverFunction)(struct RDUINode* node, enum RDUIEvent event, void* data);
+typedef void (*RDUIEventReceiverFunction)(struct RDUINode *node,
+                                          enum RDUIEvent event, void *data);
 
 struct RDUINode {
-    void* data;
+  void *data;
 
-    RDUIEventReceiverFunction event_receiver_function;
+  RDUIEventReceiverFunction event_receiver_function;
 };
 
 RDPoint RDUIMousePosition = {.x = 0, .y = 0};
 
-#define RDUIIfEventIs(if_name) \
-  struct RDUIEventData_##if_name* if_name##_event = data; \
-  if(event == RDUIEvent_##if_name)
+#define RDUIIfEventIs(if_name)                                                 \
+  struct RDUIEventData_##if_name *if_name##_event = data;                      \
+  if (event == RDUIEvent_##if_name)
 
-struct RDUIMenu* RDUIProcessedMenu = NULL;
+struct RDUIMenu *RDUIProcessedMenu = NULL;
 
 void RDUIInit() {
-    // may be removed in future
+  // may be removed in future
 }
 
-struct RDUINode* RDUINewNode(
-        void* data,
-        RDUIEventReceiverFunction event_receiver_function
-        ) {
-    struct RDUINode* node = malloc(sizeof(struct RDUINode));
+struct RDUINode *
+RDUINewNode(void *data, RDUIEventReceiverFunction event_receiver_function) {
+  struct RDUINode *node = malloc(sizeof(struct RDUINode));
 
-    node->data = data;
-    node->event_receiver_function = event_receiver_function;
+  node->data = data;
+  node->event_receiver_function = event_receiver_function;
 
-    return node;
+  return node;
 }
 
-
-void RDUINoOpEventReceiver(struct RDUINode* node, enum RDUIEvent event, void* data) {}
-
+void RDUINoOpEventReceiver(struct RDUINode *node, enum RDUIEvent event,
+                           void *data) {}
 
 struct RDUIMenu {
-	size_t size;
-	struct RDUINode** nodes;
+  size_t size;
+  struct RDUINode **nodes;
 };
 
+#define RDUIForEachNode() for (size_t i = 0; i < menu->size; i++)
 
-#define RDUIForEachNode() for(size_t i = 0; i < menu->size; i++)
+void RDUIDispatchEvent(struct RDUIMenu *menu, enum RDUIEvent event,
+                       void *data) {
+  RDUIProcessedMenu = menu;
 
-void RDUIDispatchEvent(struct RDUIMenu* menu, enum RDUIEvent event, void* data) {
-    RDUIProcessedMenu = menu;
+  RDUIForEachNode() menu->nodes[i]->event_receiver_function(menu->nodes[i],
+                                                            event, data);
 
-    RDUIForEachNode() menu->nodes[i]->event_receiver_function(menu->nodes[i], event, data);
-
-    RDUIProcessedMenu = NULL;
+  RDUIProcessedMenu = NULL;
 }
 
 // Handlers for rawdraw events
-void RDUIHandleKeyImpl(struct RDUIMenu* menu, int keycode, int bDown) {
-    struct RDUIEventData_key event = {.keycode = keycode, .bDown = bDown};
-    RDUIDispatchEvent(menu, RDUIEvent_key, &event);
+void RDUIHandleKeyImpl(struct RDUIMenu *menu, int keycode, int bDown) {
+  struct RDUIEventData_key event = {.keycode = keycode, .bDown = bDown};
+  RDUIDispatchEvent(menu, RDUIEvent_key, &event);
 }
 
-void RDUIHandleButtonImpl(struct RDUIMenu* menu, int x, int y, int button, int bDown) {
-    struct RDUIEventData_button event = {.position = {.x = x, .y = y}, .button = button, .bDown = bDown};
-    RDUIDispatchEvent(menu, RDUIEvent_button, &event);
+void RDUIHandleButtonImpl(struct RDUIMenu *menu, int x, int y, int button,
+                          int bDown) {
+  struct RDUIEventData_button event = {
+      .position = {.x = x, .y = y}, .button = button, .bDown = bDown};
+  RDUIDispatchEvent(menu, RDUIEvent_button, &event);
 }
 
-void RDUIHandleMotionImpl(struct RDUIMenu* menu, int x, int y, int mask) {
-    struct RDUIEventData_motion event = {.position = {.x = x, .y = y}, .mask = mask};
+void RDUIHandleMotionImpl(struct RDUIMenu *menu, int x, int y, int mask) {
+  struct RDUIEventData_motion event = {.position = {.x = x, .y = y},
+                                       .mask = mask};
 
-    RDUIMousePosition.x = x;
-    RDUIMousePosition.y = y;
+  RDUIMousePosition.x = x;
+  RDUIMousePosition.y = y;
 
-    RDUIDispatchEvent(menu, RDUIEvent_motion, &event);
+  RDUIDispatchEvent(menu, RDUIEvent_motion, &event);
 }
 
-struct RDUIMenu* RDUINewMenu(size_t size, ...) {
-    va_list list;
+struct RDUIMenu *RDUINewMenu(size_t size, ...) {
+  va_list list;
 
-    struct RDUIMenu* menu = malloc(sizeof(struct RDUIMenu));
-    menu->size = size;
-    menu->nodes = malloc(sizeof(struct RDUINode*) * size);
+  struct RDUIMenu *menu = malloc(sizeof(struct RDUIMenu));
+  menu->size = size;
+  menu->nodes = malloc(sizeof(struct RDUINode *) * size);
 
-    va_start(list, size);
-    for(int i = 0; i < size; i++) menu->nodes[i] = va_arg(list, struct RDUINode*);
-    va_end(list);
+  va_start(list, size);
+  for (int i = 0; i < size; i++)
+    menu->nodes[i] = va_arg(list, struct RDUINode *);
+  va_end(list);
 
-    return menu;
+  return menu;
 }
 
-void RDUIMenuPush(struct RDUIMenu* menu, struct RDUINode* node) {
-    menu->size++;
+void RDUIMenuPush(struct RDUIMenu *menu, struct RDUINode *node) {
+  menu->size++;
 
-    menu->nodes = malloc(menu->size * sizeof(struct RDUINode*));
-    if(menu->nodes != NULL) {
-        menu->nodes[menu->size - 1] = node;
-    } else {
-        menu->size--;
-    }
+  menu->nodes = malloc(menu->size * sizeof(struct RDUINode *));
+  if (menu->nodes != NULL) {
+    menu->nodes[menu->size - 1] = node;
+  } else {
+    menu->size--;
+  }
 }
 
-extern struct RDUIMenu* RDUIProcessedMenu;
+extern struct RDUIMenu *RDUIProcessedMenu;
--- examples/demo.c
+++ examples/demo.c
@@ -3,152 +3,128 @@
 #include <RDUI.h>
 #include <default-elements.h>
 #include <onlyinput/onlyinput.h>
-#include "util.h"
 #include <stdio.h>
 #include <string.h>
 
-static struct RDUIMenu* menu;
+#include "util.h"
+
+static struct RDUIMenu *menu;
 
 void HandleKey(int keycode, int bDown) {
-	OIHandleKey(keycode, bDown);
-	RDUIHandleKeyImpl(menu, keycode, bDown);
+  OIHandleKey(keycode, bDown);
+  RDUIHandleKeyImpl(menu, keycode, bDown);
 
-	printf("key %d\n", keycode);
+  printf("key %d\n", keycode);
 }
 
 void HandleButton(int x, int y, int button, int bDown) {
-	RDUIHandleButtonImpl(menu, x, y, button, bDown);
+  RDUIHandleButtonImpl(menu, x, y, button, bDown);
 }
 
 void HandleMotion(int x, int y, int mask) {
-	RDUIHandleMotionImpl(menu, x, y, mask);
+  RDUIHandleMotionImpl(menu, x, y, mask);
 }
 
 void HandleDestroy() {}
 
-void ButtonSecondClickedHandle(struct RDUIButtonData* data) {
-	exit(0);
+void ButtonSecondClickedHandle(struct RDUIButtonData *data) { exit(0); }
+
+void ButtonClickedHandler(struct RDUIButtonData *data) {
+  data->text = "Not again, please!";
+  data->clicked_handler = ButtonSecondClickedHandle;
 }
 
-void ButtonClickedHandler(struct RDUIButtonData* data) {
-	data->text = "Not again, please!";
-	data->clicked_handler = ButtonSecondClickedHandle;
+void CheckboxActivateHandler(struct RDUICheckboxData *data) {}
+
+void CheckboxDeactivateHandler(struct RDUICheckboxData *data) {}
+
+void OptionsBoxSelectHandler(struct RDUIOptionsBoxData *data) {}
+
+void FieldTypeHandler(struct RDUIFieldData *data) {
+  if (!strcmp(data->value, "example")) {
+    UtilStringChange(data->value, "demo");
+    data->cursor = 4;
+  }
 }
 
-void CheckboxActivateHandler(struct RDUICheckboxData* data) {
+int main(int argv, char *argc[]) {
+  RDUIInit();
 
-}
+  CNFGBGColor = 0xffffffff;
 
-void CheckboxDeactivateHandler(struct RDUICheckboxData* data) {
+  CNFGSetup("RDUI Example", 800, 600);
 
-}
+  struct RDUIButtonData button_data = {.text = "Click on me!",
+                                       .font_size = 6,
+                                       .padding = 5,
+                                       .color = 0x555555ff,
+                                       .font_color = 0xffffffff,
+                                       .position = {.x = 70, .y = 10},
 
-void OptionsBoxSelectHandler(struct RDUIOptionsBoxData* data) {
+                                       .clicked_handler = ButtonClickedHandler};
 
-}
+  struct RDUICheckboxData checkbox_data = {
+      .active = 0,
+      .size = 50,
+      .padding = 5,
+      .color = 0x232323ff,
+      .checkmark_color = 0xffffffff,
+      .position = {.x = 10, .y = 10},
 
-void FieldTypeHandler(struct RDUIFieldData* data) {
-	if(!strcmp(data->value, "example")) {
-	    UtilStringChange(data->value, "demo");
-	    data->cursor = 4;
-	}
-}
+      .activate_handler = CheckboxActivateHandler,
+      .deactivate_handler = CheckboxDeactivateHandler};
 
-int main(int argv, char* argc[]) {
-	RDUIInit();
+  struct RDUIFieldData field_data = {.padding = 5,
+                                     .font_size = 5,
+                                     .min_width = 300,
+                                     .max_width = 1000,
+                                     .position = {.x = 10, .y = 100},
+                                     .font_color = 0x000000ff,
+                                     .border_color = 0x000000ff,
+                                     .value = "A text field",
+                                     .allowed_symbols = NULL,
 
-	CNFGBGColor = 0xffffffff;
+                                     .type_handler = FieldTypeHandler};
 
-	CNFGSetup("RDUI Example", 800, 600);
+  char *options[] = {"Pepsi", "Coca Cola",    "Fanta",
+                     "Tea",   "Orange juice", NULL};
 
-	struct RDUIButtonData button_data = {
-		.text = "Click on me!",
-		.font_size = 6,
-		.padding = 5,
-		.color = 0x555555ff,
-		.font_color = 0xffffffff,
-		.position = {
-			.x = 70,
-			.y = 10
-		},
+  struct RDUIOptionsBoxData options_box_data = {.position = {.x = 10, .y = 150},
+                                                .color = 0x555555ff,
+                                                .font_color = 0xffffffff,
+                                                .font_size = 5,
+                                                .padding = 5,
+                                                .options = options,
 
-		.clicked_handler = ButtonClickedHandler
-	};
+                                                .select_handler =
+                                                    OptionsBoxSelectHandler};
 
-	struct RDUICheckboxData checkbox_data = {
-		.active = 0,
-		.size = 50,
-		.padding = 5,
-		.color = 0x232323ff,
-		.checkmark_color = 0xffffffff,
-		.position = {
-			.x = 10,
-			.y = 10
-		},
+  struct RDUINode *button = RDUINewButton(&button_data);
+  struct RDUINode *checkbox = RDUINewCheckbox(&checkbox_data);
+  struct RDUINode *field = RDUINewField(&field_data);
+  struct RDUINode *options_box = RDUINewOptionsBox(&options_box_data);
 
-		.activate_handler = CheckboxActivateHandler,
-		.deactivate_handler = CheckboxDeactivateHandler
-	};
+  menu = RDUINewMenu(4, button, checkbox, field, options_box);
 
-	struct RDUIFieldData field_data = {
-		.padding = 5,
-		.font_size = 5,
-		.min_width = 300,
-		.max_width = 1000,
-		.position = {
-			.x = 10,
-			.y = 100
-		},
-		.font_color = 0x000000ff,
-		.border_color = 0x000000ff,
-		.value = "A text field",
-		.allowed_symbols = NULL,
+  RDUIFocusedField = &field_data;
 
-		.type_handler = FieldTypeHandler
-	};
+  while (1) {
+    CNFGHandleInput();
+    CNFGClearFrame();
 
-	char* options[] = {"Pepsi", "Coca Cola", "Fanta", "Tea", "Orange juice", NULL};
+    CNFGColor(0x444444ff);
 
-	struct RDUIOptionsBoxData options_box_data = {
-		.position = {
-			.x = 10,
-			.y = 150
-		},
-		.color = 0x555555ff,
-		.font_color = 0xffffffff,
-		.font_size = 5,
-		.padding = 5,
-		.options = options,
+    RDUIDispatchEvent(menu, RDUIEvent_render, NULL);
+    if (options_box_data.selected_index == 2) {
+      CNFGColor(0);
+      CNFGPenX = 200;
+      CNFGPenY = 150;
 
-		.select_handler = OptionsBoxSelectHandler
-	};
+      CNFGDrawText("why", 5);
+    }
 
-	struct RDUINode* button = RDUINewButton(&button_data);
-	struct RDUINode* checkbox = RDUINewCheckbox(&checkbox_data);
-	struct RDUINode* field = RDUINewField(&field_data);
-	struct RDUINode* options_box = RDUINewOptionsBox(&options_box_data);
+    CNFGSwapBuffers();
+  }
 
-	menu = RDUINewMenu(4, button, checkbox, field, options_box);
-
-	RDUIFocusedField = &field_data;
-
-	while(1) {
-		CNFGHandleInput();
-		CNFGClearFrame();
-
-		CNFGColor(0x444444ff);
-
-		RDUIDispatchEvent(menu, RDUIEvent_render, NULL);
-		if(options_box_data.selected_index == 2) {
-			CNFGColor(0);
-			CNFGPenX = 200;
-			CNFGPenY = 150;
-
-			CNFGDrawText("why", 5);
-		}
-
-		CNFGSwapBuffers();
-	}
-
-	return 0;
+  return 0;
 }
